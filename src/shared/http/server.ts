79ef3e52a3966dac39eeef4a68b5108a9465c0f8
William santos <smurf455@hotmail.com>
2021-11-12 10:39:09 -0300

    initial commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from '@shared/routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        error: 'Error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      error: 'Error',
+      message: 'Intertal server error',
+    });
+  },
+);
+
+app.listen(3333, () => {
+  console.log('server running on port 3333');
+});
